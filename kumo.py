f0c8a5f9a478743eb26ebc7479f686e3a0be10de
Ryan Flynn <ryanflynn44@gmail.com>
2021-03-01 11:53:23 -0500

    Moved all comments to the top of the line they are explaining

--- kumo.py
+++ kumo.py
@@ -1,7 +1,7 @@
 
-
-DIGITS = '0123456789'
 #This is all our digits, and helps use determine if our char is a digit or not.
+DIGITS = '0123456789'
+
 
 class Error:
     def __init__(self, pos_start, pos_end, error_name, details):
@@ -51,44 +51,47 @@ TT_RPAREN = 'RPAREN'
 
 class Token:
     def __init__(self, type_, value=None):
+        #This makes a token with a type for example INT and a value which can be something like 5
         self.type = type_
         self.value = value
-        #This makes a token with a type for example INT and a value which can be something like 5
 
     def __repr__(self):
+        #This is just a way to represent that information. If it has a value it will go INT:5 if not it will just go PLUS for example.
         if self.value: return f'{self.type}:{self.value}'
         return f'{self.type}'
-        #This is just a way to represent that information. If it has a value it will go INT:5 if not it will just go PLUS for example.
 
 class Lexer:
     def __init__(self, fname, text):
+        #text we will be processing
         self.fname = fname
         self.text = text
-        #text we will be processing
+        #current position
         self.pos = Position(-1, 0, -1, fname, text)
-        #current pos
-        self.current_char = None 
         #current char
-        self.advance()
+        self.current_char = None 
         #We call the advance method on intialization of our lexer. This will make it start out at 0.
+        self.advance()
+        
 
     def advance(self):
-        self.pos.advance(self.current_char)
         #This advances to the next character in the text
-        self.current_char = self.text[self.pos.index] if self.pos.index < len(self.text) else None
+        self.pos.advance(self.current_char)
         #This grabs the current character only if the index is not the length of the text. If it is we set the current char to none.
+        self.current_char = self.text[self.pos.index] if self.pos.index < len(self.text) else None
+        
 
     def make_tokens(self):
-        tokens = []
         # This will hold our tokens
+        tokens = []
+        #This a loop that will got through every char what token the current char will hold as long as the current char is not None.
         while self.current_char != None: 
-            #This a loop that will got through every char what token the current char will hold as long as the current char is not None.
             if self.current_char in ' \t':
-                self.advance() 
                 #This ignores all spaces and tabs, and calls advance when it sees one
+                self.advance() 
+                #This looks to see if our char is in the digits constant, and if so it makes into a number.
             elif self.current_char in DIGITS:
                 tokens.append(self.make_number())
-                #This looks to see if our char is in the digits constant, and if so it makes into a number.
+                
             elif self.current_char == '+':
                 tokens.append(Token(TT_PLUS))
                 self.advance()
@@ -116,26 +119,32 @@ class Lexer:
         return tokens, None 
 
     def make_number(self):
+        #Here we are making the string representation of our number, and keeping track of how many decimals there are
         num_str = ''
         dot_count = 0
-        #Here we are making the string representation of our number, and keeping track of how many decimals there are
+        
+        #Here we are making sure the current char exists, and that is either a digit or a .
         while self.current_char != None and self.current_char in DIGITS + '.':
-            #Here we are making sure the current char exists, and that is either a digit or a .
+            
             if self.current_char == '.':
+                #If we find a dot we increment our dot count, and we add the "." to our string repr of our number. If there is more than one we break, because a number can't have more than one decimal.
                 if dot_count == 1: break
                 dot_count += 1
                 num_str += '.'
-                #If we find a dot we increment our dot count, and we add the "." to our string repr of our number. If there is more than one we break, because a number can't have more than one decimal.
+
             else:
-                num_str += self.current_char
                 #if it is not a decimal/dot then we just append the current char to our num string
+                num_str += self.current_char
+                
             self.advance()
         if dot_count == 0:
-            return Token(TT_INT, int(num_str))
             #if there are no decimals our char is an integer
+            return Token(TT_INT, int(num_str))
+            
         else:
-            return Token(TT_FLOAT, float(num_str))
             #if there are decimals then our char is a float
+            return Token(TT_FLOAT, float(num_str))
+            
 
 
 class NumberNode: 
